7b8279158e562d8a2e2de85e6d2472482b7ffba7
justintangjx <just.think.tang@gmail.com>
2020-08-24 18:21:32 +0800

    incomplete slow working gradient descent model w/o MSE

---
+++ regressions/linearRegression.js
@@ -0,0 +1,28 @@
+const tf = require('@tensorflow/tfjs');
+
+class LinearRegression {
+    constructor(features, labels, options) {
+        this.features = features;
+        this.labels = labels;
+
+        this.options = Object.assign({ learningRate: 0.1 }, options);
+
+        this.m = 0;
+        this.b = 0;
+    }
+
+    gradientDescent() {
+        const currentGuessesForMPG = this.features.map(row => {
+            return this.m * row[0] + this.b;
+        })
+    }
+
+    train() {
+        for (let i = 0; i < this.options.iterations; i++) {
+            this.gradientDescent();
+        }
+    }
+
+}
+
+module.exports = LinearRegression;
